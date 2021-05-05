bd22265587a8b89b9a4b8f2a457c19306c454d65
joewickes <joe.c.wickes@gmail.com>
2021-05-05 13:48:49 -0400

    Test how scope works with multi-variable assignment and anonymous functions

--- arithmetic-operators.js
+++ arithmetic-operators.js
@@ -1,27 +1,36 @@
-/**
-*   Calculate the area of a rectangle.
-*
-*   length: The length of the rectangle.
-*   width: The width of the rectangle.
-*   
-*	Return a number denoting the rectangle's area.
-**/
-function getArea(length, width) {
-  const area = length * width;
+// /**
+// *   Calculate the area of a rectangle.
+// *
+// *   length: The length of the rectangle.
+// *   width: The width of the rectangle.
+// *   
+// *	Return a number denoting the rectangle's area.
+// **/
+// function getArea(length, width) {
+//   const area = length * width;
   
-  return area;
-}
+//   return area;
+// }
 
-/**
-*   Calculate the perimeter of a rectangle.
-*	
-*	length: The length of the rectangle.
-*   width: The width of the rectangle.
-*   
-*	Return a number denoting the perimeter of a rectangle.
-**/
-function getPerimeter(length, width) {
-  const perimeter = (length * 2) + (width * 2);
+// /**
+// *   Calculate the perimeter of a rectangle.
+// *	
+// *	length: The length of the rectangle.
+// *   width: The width of the rectangle.
+// *   
+// *	Return a number denoting the perimeter of a rectangle.
+// **/
+// function getPerimeter(length, width) {
+//   const perimeter = (length * 2) + (width * 2);
   
-  return perimeter;
-}
+//   return perimeter;
+// }
+
+(function() {
+  var a = b = 3;
+  console.log('a', a);
+  console.log('b', b);
+})();
+
+console.log(a);
+console.log(b);
